24925f572d38bad73f01a6379e4e7bd5b6cc7a40
Agustin <perezp.agustin@gmail.com>
2023-02-27 12:17:42 -0300

    fix: add a project

--- src/mock/latestProjects.js
+++ src/mock/latestProjects.js
@@ -1,5 +1,6 @@
 import WomboPreviewImg from '~/assets/wombo_preview_img.png'
 import AguspdevPreviewImg from '~/assets/aguspdev_preview_img.png'
+import PostProcesosPreviewImg from '~/assets/postprocesos_img.png'
 
 export const latestProjectsData = {
   title: 'Latest Projects',
@@ -28,6 +29,18 @@ export const latestProjectsData = {
     myWork: 'Completely build the site with Vite.js. To use chakra-ui with Emotion for styling.',
     url: 'https://www.agusp.dev',
     techStack: ['Vite.js', 'React.js', 'Javascript', 'Chakra UI', 'Emotion', 'Atomic Design'],
+    githubUrl: '',
+  }, {
+    id: '3',
+    title: 'Postprocesos Landing Page',
+    previewImg: PostProcesosPreviewImg,
+    company: 'Postprocesos',
+    startDate: '19/02/2022',
+    endDate: '03/04/2022',
+    shortDescription: 'Postprocesos landing page',
+    myWork: 'I collaborated on the project defining the technologies and styling to integrate.',
+    url: 'http://postprocesos.com/',
+    techStack: ['React.js', 'Javascript', 'Chakra UI', 'Emotion', 'Atomic Design'],
     githubUrl: '',
   }]
 }
