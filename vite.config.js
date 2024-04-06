0877799e1e7ed4a93e2577033473536a89199329
Agustin <perezp.agustin@gmail.com>
2024-04-06 20:02:37 -0300

    fix: add production vite base config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://agusp.dev',
   plugins: [react()],
   resolve: {
     alias: [
       { find: '~', replacement: path.resolve(__dirname, './src') }
     ]
-  }
+  },
 })
