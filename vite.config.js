1beab879f782e4a721ae650d114acf74481564c5
Agustin <perezp.agustin@gmail.com>
2024-04-07 19:10:09 -0300

    fix: add vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: 'https://agusp.dev',
+  base: 'https://agusp.dev/',
   plugins: [react()],
   resolve: {
     alias: [
